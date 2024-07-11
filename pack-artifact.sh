7a3137d767f48cea3b5f79b71bada03d9f6b0912
Yiyun Liu <liuyiyun@seas.upenn.edu>
2024-07-11 15:47:30 -0400

    Update pack script for both impl and proofs

--- pack-artifact.sh
+++ pack-artifact.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 rm -rf artifact artifact.tar
 git archive --prefix='artifact/' --format=tar HEAD proofs | tar -xf - --exclude=LICENSE
+cd system-dcr/pi-forall
+git archive --prefix='artifact/impl/' --format=tar HEAD | tar -xf - --exclude=.github --exclude=LICENSE  -C ../../
+cd ../..
 tar cf artifact.tar artifact
 rm -r artifact
