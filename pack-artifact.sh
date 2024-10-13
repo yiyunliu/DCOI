cc86d37069c9b8c7254de1c796cf564cc1cd6379
Yiyun Liu <liuyiyun@seas.upenn.edu>
2024-10-12 23:36:44 -0400

    Update packing script

--- pack-artifact.sh
+++ pack-artifact.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
+set -xe
+
 rm -rf artifact artifact.tar
-git archive --prefix='artifact/' --format=tar HEAD proofs | tar -xf - --exclude=LICENSE
+git archive --prefix='artifact/' --format=tar HEAD proofs | tar -xf -
 cd system-dcr/pi-forall
-git archive --prefix='artifact/impl/' --format=tar HEAD | tar -xf - --exclude=.github --exclude=LICENSE  -C ../../
+git archive --prefix='artifact/impl/' --format=tar HEAD | tar -xf - --exclude=.github  -C ../../
 cd ../..
 cp reference-appendix.pdf artifact/
 tar cf artifact.tar artifact
